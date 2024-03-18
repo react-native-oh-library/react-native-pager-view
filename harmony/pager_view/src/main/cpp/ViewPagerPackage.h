019fa7c1f1977460c84629a357b39f3044b7cef9
wupingyuan <3415239542@qq.com>
2024-03-18 13:47:53 +0800

    更新har和版本号

--- harmony/pager_view/src/main/cpp/ViewPagerPackage.h
+++ harmony/pager_view/src/main/cpp/ViewPagerPackage.h
@@ -51,7 +51,7 @@ namespace rnoh{
       ViewPagerPackage(Package::Context ctx) : Package(ctx){}
 
       ComponentInstanceFactoryDelegate::Shared createComponentInstanceFactoryDelegate() override {
-            return std::make_shared<ViewPagerPackageComponentInstanceFactoryDelegate>();
+            return std::make_shared<ViewPagerPackageComponentInstanceFactoryDelegate>(m_ctx);
       }
 
       std::vector<facebook::react::ComponentDescriptorProvider> createComponentDescriptorProviders() override
